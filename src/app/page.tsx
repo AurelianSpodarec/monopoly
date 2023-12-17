9c11cd7011d8040510b562bf8b18918035e48b00
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2023-12-17 22:31:30 +0100

    feat: Added prperty tile with reversed Y axies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 
 import Image from 'next/image'
 
-import { FlagBrazil, FlagIsrael } from './flags';
+import { FlagBrazil, FlagFrance, FlagIsrael } from './flags';
 import dataProperties from '../data/properties';
 import { dataTransport } from '../data/transport';
 import { dataTax } from '../data/tax';
@@ -60,6 +60,9 @@ const dataSpecial = {
 }
 
 const dataBoard: any = [
+  {
+    name: "GO"
+  },
   {
     ...dataProperties.brazil,
     ...dataProperties.brazil.properties[0],
@@ -124,7 +127,7 @@ const dataBoard: any = [
     ...dataProperties.germany.properties[0],
   },
   {
-    name: "Suprise",
+    name: "Chest",
     icon: "./images/chest.png"
   },
   {
@@ -137,6 +140,79 @@ const dataBoard: any = [
   },
   {
     name: "Vacation"
+  },
+  {
+    ...dataProperties.china,
+    ...dataProperties.china.properties[0],
+  },
+  {
+    name: "Suprise",
+    icon: "./images/question-mark.png"
+  },
+  {
+    ...dataProperties.china,
+    ...dataProperties.china.properties[1],
+  },
+  {
+    ...dataProperties.china,
+    ...dataProperties.china.properties[2],
+  },
+  {
+    icon: dataTransport.icon,
+    ...dataTransport.properties[2]
+  },
+  {
+    ...dataProperties.france,
+    ...dataProperties.france.properties[0],
+  },
+  {
+    ...dataProperties.france,
+    ...dataProperties.france.properties[1],
+  },
+  {
+    name: "Company"
+  },
+  {
+    ...dataProperties.france,
+    ...dataProperties.france.properties[2],
+  },
+  {
+    name: "Go to prison"
+  },
+  {
+    ...dataProperties.england,
+    ...dataProperties.england.properties[0],
+  },
+  {
+    ...dataProperties.england,
+    ...dataProperties.england.properties[1],
+  },
+  {
+    name: "Chest",
+    icon: "./images/chest.png"
+  },
+  {
+    ...dataProperties.england,
+    ...dataProperties.england.properties[2],
+  },
+  {
+    icon: dataTransport.icon,
+    ...dataTransport.properties[3]
+  },
+  {
+    name: "Suprise",
+    icon: "./images/question-mark.png"
+  },
+  {
+    ...dataProperties.usa,
+    ...dataProperties.usa.properties[0]
+  },
+  {
+    name: "Luxury Tax",
+  },
+  {
+    ...dataProperties.usa,
+    ...dataProperties.usa[1]
   }
 ]
 
@@ -151,41 +227,136 @@ function Row() {
   )
 }
 
-function Tile({ tile }: any) {
+
+interface Tile {
+  tile: any;
+  direction: "reverse";
+}
+
+// Tile: Property, Chest, Suprise, Tax, Transport
+function Tile({ tile, direction, reverse }: any) {
   return (
-    <div className="flex items-center justify-between rounded-lg relative flex-col overflow-hidden" style={{ blockSize: "6.5rem", flex: "1 0 0" }}>
-      {/* <div>Colors</div> */}
-      <div className="absolute flex items-center justify-center top-0 right-0 bottom-0 left-0 opacity-20 pointer-events-none">
+    <div className={`flex items-center text-white bg-[#262a4c] cursor-pointer justify-between rounded-lg relative flex-col ${reverse ? "flex-col-reverse" : "flex-col"}`} style={{ blockSize: "6.5rem", flex: "1 0 0", minInlineSize: "4.25rem" }}>
+
+      <div className="absolute overflow-hidden flex items-center justify-center top-0 right-0 bottom-0 left-0 opacity-20 pointer-events-none" style={{ borderRadius: "inherit" }}>
         <div className="blur-sm inset-0 items-center flex justify-center absolute cheat-svg-icon">
           {tile.icon}
         </div>
       </div>
-      <img src={tile.icon} />
 
-      <div className="z-20">
-        {/* <div>
-          100$
-        </div> */}
-        <p>{tile?.name}</p>
-        <div className="h-8 w-8 rounded-full overflow-hidden">
+      <div className={`z-10 flex items-center flex-col ${reverse ? "flex-col-reverse" : "flex-col"}`}>
+        <div className="h-8 w-8 rounded-full overflow-hidden" style={{ marginBlockEnd: "-1rem", marginBlockStart: "-1rem" }}>
           {tile.icon}
-
+        </div>
+        <div className="items-center leading-3" style={{ marginBlockEnd: "1.5rem", marginBlockStart: "1.5rem", textShadow: "0 0 0.5em #262a4c" }}>
+          <p>{tile?.name}</p>
         </div>
       </div>
+      <div className="bg-white/20 py-1 text-white px-2 m-1 font-semibold rounded text-xs">
+        280$
+      </div>
+
     </div>
   )
 }
 
+
+
+
 function Board() {
-  console.log("hi", dataBoard)
   return (
-    <div className="flex justify-evenly max-w-3lg">
-      {dataBoard.map((tile) => {
-        return <Tile tile={tile} />
-      })}
+    <div className='flex'>
+      <Tile
+        tile={{
+          name: "Paris",
+          icon: <FlagFrance />
+        }}
+      />
+      <Tile
+        reverse
+        tile={{
+          name: "Paris",
+          icon: <FlagFrance />
+        }}
+      />
+      {/* <Tile
+      reverse
+        tile={{
+          name: "Test",
+          icon: <FlagBrazil />
+        }} 
+      /> */}
     </div>
   )
 }
+
+
+// function Board() {
+//   console.log("hi", dataBoard);
+
+//   // Function to determine the class based on the index
+//   const getRowClass = (index: number) => {
+//     const rowNumber = Math.floor(index / 10) + 1;
+
+//     switch (rowNumber) {
+//       case 1:
+//         return 'top';
+//       case 2:
+//         return 'right';
+//       case 3:
+//         return 'bottom';
+//       default:
+//         return 'left';
+//     }
+//   };
+
+//   const getTileClass = (index: number) => {
+//     if ((index + 1) % 10 === 1) {
+//       const cornerIndex = Math.floor(index / 10);
+//       switch (cornerIndex) {
+//         case 0:
+//           return 'start';
+//         case 1:
+//           return 'prison';
+//         case 2:
+//           return 'vacation';
+//         case 3:
+//           return 'gotoprison';
+//         default:
+//           return '';
+//       }
+//     }
+//     return '';
+//   };
+
+//   return (
+//     <div className="grid justify-evenly max-w-3lg board">
+//       {dataBoard.map((tile: any, index: number) => {
+//         const tileClass = getTileClass(index);
+
+//         // If index is a multiple of 10, add a div with specific class
+//         if (index % 10 === 0) {
+//           const rowClass = getRowClass(index);
+//           const tilesInRow = dataBoard.slice(index, index + 10);
+
+//           return (
+//             <div key={index} className={`row board-${rowClass} ${tileClass}`}>
+//               {tilesInRow.map((tileInRow: any, tileIndex: number) => 
+//                 <Tile key={tileIndex} tile={tileInRow} className={tileClass} />
+//               )}
+//             </div>
+//           );
+//         }
+
+//         return null; // Skip rendering for other indexes
+//       })}
+//     </div>
+//   );
+// }
+
+
+
+
 
 export default function Home() {
   return (
