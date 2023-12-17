569375b05adc8efca373e8eea96da7d26af6f981
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2023-12-17 23:59:05 +0100

    feat: Added directions to property tiles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -235,17 +235,34 @@ interface Tile {
 
 // Tile: Property, Chest, Suprise, Tax, Transport
 function Tile({ tile, direction, reverse }: any) {
+
+  const directions: any = {
+    left: "[writing-mode:vertical-rl] rotate-180",
+    right: "[writing-mode:vertical-lr] rotate-270"
+  }
+
+  const flagDirection: any = {
+    left: "rotate-90",
+    right: "rotate-90 "
+  }
+
   return (
-    <div className={`flex items-center text-white bg-[#262a4c] cursor-pointer justify-between rounded-lg relative flex-col ${reverse ? "flex-col-reverse" : "flex-col"}`} style={{ blockSize: "6.5rem", flex: "1 0 0", minInlineSize: "4.25rem" }}>
+    <div className={`
+      flex items-center text-white bg-[#262a4c] cursor-pointer justify-between rounded-lg relative flex-col 
+      ${reverse ? "flex-col-reverse" : "flex-col"}
+      ${direction && directions[direction]}
+    `}
+      style={{ blockSize: "6.5rem", flex: "1 0 0", minInlineSize: "4.25rem" }}
+    >
 
       <div className="absolute overflow-hidden flex items-center justify-center top-0 right-0 bottom-0 left-0 opacity-20 pointer-events-none" style={{ borderRadius: "inherit" }}>
-        <div className="blur-sm inset-0 items-center flex justify-center absolute cheat-svg-icon">
+        <div className={`blur-sm inset-0 items-center flex justify-center absolute cheat-svg-icon ${direction && flagDirection[direction]}`}>
           {tile.icon}
         </div>
       </div>
 
       <div className={`z-10 flex items-center flex-col ${reverse ? "flex-col-reverse" : "flex-col"}`}>
-        <div className="h-8 w-8 rounded-full overflow-hidden" style={{ marginBlockEnd: "-1rem", marginBlockStart: "-1rem" }}>
+        <div className={`h-8 w-8 rounded-full overflow-hidden ${direction && flagDirection[direction]}`} style={{ marginBlockEnd: "-1rem", marginBlockStart: "-1rem" }}>
           {tile.icon}
         </div>
         <div className="items-center leading-3" style={{ marginBlockEnd: "1.5rem", marginBlockStart: "1.5rem", textShadow: "0 0 0.5em #262a4c" }}>
@@ -265,27 +282,57 @@ function Tile({ tile, direction, reverse }: any) {
 
 function Board() {
   return (
-    <div className='flex'>
-      <Tile
-        tile={{
-          name: "Paris",
-          icon: <FlagFrance />
-        }}
-      />
-      <Tile
-        reverse
-        tile={{
-          name: "Paris",
-          icon: <FlagFrance />
-        }}
-      />
-      {/* <Tile
-      reverse
-        tile={{
-          name: "Test",
-          icon: <FlagBrazil />
-        }} 
-      /> */}
+    <div>
+
+      <div className='flex'>
+        <Tile
+          tile={{
+            name: "Paris",
+            icon: <FlagFrance />
+          }}
+        />
+        <Tile
+          reverse
+          tile={{
+            name: "Paris",
+            icon: <FlagFrance />
+          }}
+        />
+      </div>
+      <div className="flex flex-col">
+        <Tile
+          direction="left"
+          tile={{
+            name: "Paris",
+            icon: <FlagFrance />
+          }}
+        />
+        <Tile
+          direction="left"
+          tile={{
+            name: "Paris",
+            icon: <FlagFrance />
+          }}
+        />
+      </div>
+      <div className="flex flex-col">
+        <Tile
+          reverse
+          direction="right"
+          tile={{
+            name: "Paris",
+            icon: <FlagFrance />
+          }}
+        />
+        <Tile
+          reverse
+          direction="right"
+          tile={{
+            name: "Paris",
+            icon: <FlagFrance />
+          }}
+        />
+      </div>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,9 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safelist: [
+    "rotate-90", "-rotate-90"
+  ],
   theme: {
     extend: {
       backgroundImage: {
